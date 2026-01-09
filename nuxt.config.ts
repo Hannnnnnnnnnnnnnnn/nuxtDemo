d45fcc951e56d004f875a4068e379fa22003dcdf
Hannnnnnnnnnnnnnnn <1804175014@qq.com>
2026-01-10 00:10:30 +0800

    feat: 本地支持调试ssr和csr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  ssr: process.env.NUXT_MODE === 'ssr',
+  devServer: {
+    port: process.env.NUXT_PORT ? parseInt(process.env.NUXT_PORT) : 3000
+  },
 })
